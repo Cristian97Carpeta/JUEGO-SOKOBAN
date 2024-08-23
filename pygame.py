8cedcdafe37008120b0c7de5a96bc56a641ed278
CRISTIAN CARPETA <cristian.carpeta@yahoo.com>
2024-08-22 22:24:58 -0500

    ok

--- pygame.py
+++ pygame.py
@@ -1,22 +1,36 @@
-import pygame
-from paredes import Cubo
 
-ANCHO = 1000
-ALTO = 800
+import pygame
+from paredes import Pared
+
+ANCHO = 1000 #ancho de la ventana
+ALTO = 800 #alto de la ventana
 VENTANA = pygame.display.set_mode
 ([ANCHO,ALTO])
 
-jugando = True
-cubo = Cubo(100,100)
+jugando = True #mientras sea True se estará ejecutando el juego
+pared = Pared (100,100)
 
 while jugando:
     eventos = pygame.event.get()
 
     for evento in eventos:
         if evento.type == pygame.QUIT:
-            jugando = False
-    cubo.dibujar(VENTANA)
+            jugando = False # cuando sea False dejará de ejecutar el juego 
+    pared.dibujar(VENTANA)
     pygame.display.update()
 
 quit()
+
+def is_valid_value(self,char):
+        if ( char == ' ' or #piso
+            char == '#' or #muro
+            char == '@' or #trabajador en el piso
+            char == '.' or #muelle  
+            char == '*' or #caja en el muelle
+            char == '$' or #caja
+            char == '+' ): #trabajador en el muelle
+            return True
+        else:
+            return False
+
 
